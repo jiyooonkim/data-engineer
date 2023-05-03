ff82d85910a5474cefd1a981382abe849d793187
jiyoooon <wldbs9259@gmail.com>
2023-05-04 01:16:34 +0900

    dev skip-gram

--- commerce/cmc/etc/smtp.py
+++ commerce/cmc/etc/smtp.py
@@ -1,8 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+# title : smtp 를 통한 메일 알림 개발 (for Airflow)
+# desc :
+"""
 import smtplib
 from email.message import EmailMessage
 
 # STMP 서버의 url과 port 번호
 SMTP_SERVER = 'smtp.gmail.com'
+
 SMTP_PORT = 465
 
 smtp = smtplib.SMTP('smtp.gmail.com', 587)
@@ -10,8 +16,7 @@ smtp = smtplib.SMTP('smtp.gmail.com', 587)
 smtp.ehlo()
 
 smtp.starttls()
-EMAIL_ADDR = 'wldbs9259@gmail.com'
-EMAIL_PASSWORD = 'adazxisymteatuyu'
+EMAIL_ADDR = 'write email....'
 
 # 2. SMTP 서버에 로그인
 smtp.login(EMAIL_ADDR, EMAIL_PASSWORD)
@@ -20,8 +25,8 @@ smtp.login(EMAIL_ADDR, EMAIL_PASSWORD)
 message = EmailMessage()
 message.set_content('이메일 본문')
 message["Subject"] = "이메일 제목"
-message["From"] = EMAIL_ADDR  #보내는 사람의 이메일 계정
-message["To"] ='wldbs9259@gmail.com'
+message["From"] = EMAIL_ADDR  # 보내는 사람의 이메일 계정
+message["To"] = 'write email....'
 
 # 4. 서버로 메일 보내기
 smtp.send_message(message)
