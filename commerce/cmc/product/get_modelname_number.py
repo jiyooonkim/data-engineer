5ee3794137c2ba502b9b346c959d784fbe205224
jiyooonkim <wldbs9259@gamil.com>
2023-07-07 00:50:40 +0900

    Dev modelnumber dictionary

--- commerce/cmc/product/get_modelname_number.py
+++ commerce/cmc/product/get_modelname_number.py
@@ -69,29 +69,27 @@ if __name__ == "__main__":
 
     # Step1. 상품명에서 영+숫 타입 토큰 확보
     prod_token = spark.read.csv('/Users/jy_kim/Documents/private/nlp-engineer/commerce/data/nvr_prod.csv')\
-        .select(
+        .select(F.col('_c1'), 
             F.regexp_replace(
-                F.regexp_replace(F.lower(F.col('_c1')), "[^A-Za-z0-9]", ' '),  # 숫자, 영어(소문자)만 남김
+                F.regexp_replace(F.lower(F.col('_c1')), "[^A-Za-z0-9-]", ' '),  # 숫자, 영어(소문자)만 남김
                 r"\s+", ' '  # 다중 공백 제거
             ).alias('prod_nm_cndd'),
-        F.explode(
-            F.split(
-                F.trim(
-                    F.regexp_replace(
-                        F.regexp_replace(F.lower(F.col('_c1')), "[^A-Za-z0-9]", ' '),  # 숫자, 영어(소문자ㅊ)만 남김
-                        r"\s+", ' '     # 다중 공백 제거
-                    )
-                ), " "
-            )
-        ).alias('token')
+            F.explode(
+                F.split(
+                    F.trim(
+                        F.regexp_replace(
+                            F.regexp_replace(F.lower(F.col('_c1')), "[^A-Za-z0-9-]", ' '),  # 숫자, 영어(소문자ㅊ)만 남김
+                            r"\s+", ' '     # 다중 공백 제거
+                        )
+                    ), " "
+                )
+            ).alias('token')
     ).where(
         (F.length(F.col('token')) > 4)        # over 4 length
     ).distinct().alias('prod_token')
 
-    # prod_token.where(F.col('token').cast('string').isNotNull()).show(1000, False)
-
     attr = spark.read.parquet('/Users/jy_kim/Documents/private/nlp-engineer/data/parquet/measures_attribution').alias('attr')
-    a = prod_token\
+    get_token_info = prod_token\
         .join(
             attr,
             F.col('prod_token.token') == F.col('attr.shp_nm_token'),
@@ -99,23 +97,37 @@ if __name__ == "__main__":
         ).withColumn(
             'tps',
             get_spelling_type(F.col('token'))[0]
-        ).withColumn('num_cnt', get_text_tpye_count(get_spelling_type(F.col('token'))[1])[1])\
-        .withColumn('eng_cnt', get_text_tpye_count(get_spelling_type(F.col('token'))[1])[2])\
-        .withColumn('kor_cnt', get_text_tpye_count(get_spelling_type(F.col('token'))[1])[3])\
-        .withColumn('etc_cnt', get_text_tpye_count(get_spelling_type(F.col('token'))[1])[4])
+        ).withColumn(
+            'word_tp',
+            get_text_tpye_count(get_spelling_type(F.col('token'))[1])[0]
+        ).withColumn(
+            'num_cnt',
+            get_text_tpye_count(get_spelling_type(F.col('token'))[1])[1]
+        ).withColumn(
+            'eng_cnt',
+            get_text_tpye_count(get_spelling_type(F.col('token'))[1])[2]
+        ).withColumn(
+            'kor_cnt',
+            get_text_tpye_count(get_spelling_type(F.col('token'))[1])[3]
+        ).withColumn(
+            'etc_cnt',
+            get_text_tpye_count(get_spelling_type(F.col('token'))[1])[4]
+        )
+
+    model_nm_cndd_1 = get_token_info.where((F.col('word_tp').like('eng %')) & (F.col('num_cnt') > 2) & (F.col('eng_cnt') > 2)).select(F.col('_c1').alias('prod_nm'), F.col('token').alias('model_nm'))
+    model_nm_cndd_2 = get_token_info.where(
+        (~(F.col('word_tp').like('% eng'))) & ~(F.col('word_tp').like('num %')) & (F.col('num_cnt') > 2) & (
+                    F.col('eng_cnt') > 1)).where((F.col('eng_cnt') > 1)).select(F.col('_c1').alias('prod_nm'),
+                                                                                F.col('token').alias(
+                                                                                    'model_nm'))    # todo : 첫글자, 마지막글자 -(dash) 인것 삭제
+
+    # 번외 : 도량형 속성 추출 : eng , num 개수로 판별 하기
+    get_attr = get_token_info.where((~(F.col('word_tp').like('% eng'))) & ~(F.col('word_tp').like('num %')) & (F.col('num_cnt') > 2) & (F.col('eng_cnt') > 1)).where((F.col('eng_cnt')>1)).select(F.col('_c1').alias('prod_nm'), F.col('token').alias('model_nm')).sample(0.5)
+    get_attr.show(1000, False)
 
 
     '''
         todo : 숫자로만 이루어진 토큰으로 ngram
         조건 : 공백 전 후로만 
-    '''
-    # a.where(F.size(F.col('tps')) == 1).where(F.col('tps')[0] == 'num').orderBy(F.col('token')).show(10000, False)
-    a.where(F.col('tps')[0] == 'num').orderBy(F.col('token')).show(10000, False)
-
-    # # 도량형 속성 제거 : eng , num 개수로 판별 하기
-    # vv = a.where(F.col('tps')[0] == 'num').where((F.col('num_cnt')>1)).where((F.col('eng_cnt')>1)).sample(0.5)
-    # vv.show(100, False)
-    # # attr.where(F.col('shp_nm_token').like('%p')).show()
-
-
+    '''  
     exit(0)
