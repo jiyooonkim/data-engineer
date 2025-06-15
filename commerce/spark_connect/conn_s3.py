e9643cf1ff80ec6f94d013745f9e66ca4385c189
jiyoooon <wldbs9259@gmail.com>
2025-06-15 15:42:28 +0900

    Develop connection mysql, ncp Example

--- commerce/spark_connect/conn_s3.py
+++ commerce/spark_connect/conn_s3.py
@@ -8,7 +8,7 @@ import os
 from main import upload_file
 now_dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 service_name = 's3'
-endpoint_url = 'https://kr.object.gov-ncloudstorage.com'
+endpoint_url = 'https://kr.object.*'
 region_name = ''
 access_key = ''
 secret_key = ''
@@ -21,33 +21,34 @@ parquet_local_path = '/'
 
 if __name__ == "__main__":
     spark = (SparkSession.builder
-        .master("local[8]")
-        .config('spark.driver.memory', "16g")
-        .config('spark.driver.cores', 8)
-        .config('spark.executor.memory', "16g")
-        .config("spark.network.timeout", 100000)
-        .config("spark.hadoop.fs.s3a.access.key", access_key)
-        .config("spark.hadoop.fs.s3a.secret.key", secret_key)
-        .config("spark.hadoop.hive.exec.dynamic.partition.mode", "nonstrict")
-        .config("spark.driver.bindAddress", "127.0.0.1")
-        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-        .config("spark.sql.debug.maxToStringFields", "100000")
-        .config("spark.hadoop.fs.s3a.endpoint", endpoint_url)
-        .config("com.amazonaws.services.s3.enableV4", "true")
-        .config("spark.shuffle.encryption.enabled", False)
-        .config("spark.driver.extraJavaOptions", "-Duser.timezone=UTC")
-        .config('spark.sql.shuffle.partitions', '800')
-        .config("spark.sql.sources.partitionColumnTypeInference.enabled", False)
-        .config('spark.hadoop.fs.s3a.aws.credentials.provider',
-        'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider')
-        .appName('transform_Job')
-        .getOrCreate())
+                .master("local[8]")
+                .config('spark.driver.memory', "16g")
+                .config("spark.driver.cores", 8)
+                .config('spark.executor.memory', "16g")
+                .config("spark.network.timeout", 100000)
+                .config("spark.hadoop.fs.s3a.access.key", access_key)
+                .config("spark.hadoop.fs.s3a.secret.key", secret_key)
+                .config("spark.hadoop.hive.exec.dynamic.partition.mode", "nonstrict")
+                .config("spark.driver.bindAddress", "127.0.0.1")
+                .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+                .config("spark.sql.debug.maxToStringFields", "100000")
+                .config("spark.hadoop.fs.s3a.endpoint", endpoint_url)
+                .config("com.amazonaws.services.s3.enableV4", "true")
+                .config("spark.shuffle.encryption.enabled", False)
+                .config('spark.driver.extraJavaOptions', "-Duser.timezone=UTC")
+                .config('spark.sql.shuffle.partitions', '800')
+                .config("spark.sql.sources.partitionColumnTypeInference.enabled", False)
+                .config('spark.hadoop.fs.s3a.aws.credentials.provider',
+                'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider')
+                .appName('transform_Job')
+                .getOrCreate()
+            )
     sc = spark.sparkContext
 
     tg_df = spark.read.option("header", True).option("delimiter", '\t').csv(input_tg)\
         .select(
-        F.col('s3_path'),
-        F.col('file_name'),
-        F.col('old_table_name'),
-        F.col('new_table_name'),
+            F.col('s3_path'),
+            F.col('file_name'),
+            F.col('old_table_name'),
+            F.col('new_table_name'),
         ).alias('tg_df')
